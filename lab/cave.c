a5cf201de3f3af97e28b26310957bd91f55f28b8
Nitcharat <colab.kmitl@gmail.com>
2025-08-17 22:38:42 +0700

    Fix: add i condition

--- lab/cave.c
+++ lab/cave.c
@@ -6,7 +6,7 @@ int main() {
 
     int sum = 0;
     for (int i = 0; str[i] != '\0'; ++i) {
-        if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' || str[i] == 'o' || str[i] == 'O' || str[i] == 'u' || str[i] == 'U') {
+        if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' || str[i] == 'E' || str[i] == 'i' || str[i] == 'I' || str[i] == 'o' || str[i] == 'O' || str[i] == 'u' || str[i] == 'U') {
             ++sum;
         }
     }
